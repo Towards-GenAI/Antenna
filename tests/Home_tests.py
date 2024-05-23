58223d4ead532d2d5faceba46c7099009a1135fa
tushar27 <tushar.27041994@gmail.com>
2024-05-24 02:59:22 +0530

    model, flow tested

--- tests/Home_tests.py
+++ tests/Home_tests.py
@@ -1,5 +1,5 @@
 ##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
-##Antenna[Towards-GenAI] (https://github.com/Towards-GenAI)
+##Antenna by [Towards-GenAI] (https://github.com/Towards-GenAI)
 ##################################################################################################
 #Importing dependencies
 import os
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import logging
 from PIL import Image
+import streamlit as st
 ##################################################################################################
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -21,14 +22,14 @@ load_dotenv()
 # genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-google_api_key = os.getenv("GOOGLE_API_KEY")
+# google_api_key = os.getenv("GOOGLE_API_KEY")
 
 ##################################################################################################
 #Check if api key loaded successfully with logging info
-if google_api_key:
-    logger.info("Google API Key loaded successfully.")
-else:
-    logger.error("Failed to load Google API Key.")
+# if google_api_key:
+#     logger.info("Google API Key loaded successfully.")
+# else:
+#     logger.error("Failed to load Google API Key.")
 
 ##################################################################################################
 #Testing Gemini Pro Text Generation with logging info
@@ -157,13 +158,66 @@ def text_chain_test_with_config():
         return False
 
 # Uncomment to test
-text_chain_test_with_config()
+# text_chain_test_with_config()
 ##################################################################################################
 
 #Testing Gemini Pro text with chain with logging info & Configuring the parameters
 
+#Testing Application
+st.set_page_config(page_title="Antenna",page_icon="♊")
+st.write("Welcome to the Gemini Pro Dashboard. You can proceed by providing your Google API Key")
+
+st.sidebar.image('./src/logo.png')
+with st.sidebar.expander("Provide Google API Key please"):
+     google_api_key = st.text_input("Google API Key", key="google_api_key", type="password")
+     
+if google_api_key:
+    
+    logger.info("Google API Key loaded successfully.")
+     
+if not google_api_key:
+    st.info("Enter the Google API Key to continue")
+    st.stop()
+    
+genai.configure(api_key=google_api_key)
 
 
+###################################################################################
+#Multiplemodel selector
+with st.sidebar:
+    option = st.selectbox('Choose Your Model',('gemini-pro', 'gemini-pro-vision'))
+
+    if 'model' not in st.session_state or st.session_state.model != option:
+        st.session_state.chat = genai.GenerativeModel(option).start_chat(history=[])
+        st.session_state.model = option
+    
+    st.write("Adjust Your Parameter Here:")
+    temperature = st.number_input("Temperature", min_value=0.0, max_value= 1.0, value =0.5, step =0.01)
+    max_token = st.number_input("Maximum Output Token", min_value=0, value =100)
+    gen_config = genai.types.GenerationConfig(max_output_tokens=max_token,temperature=temperature)
+
+    st.divider()
+
+    st.markdown("<span ><font size=1>Connect With Me</font></span>",unsafe_allow_html=True)
+    "[Linkedin](https://www.linkedin.com/in/tusharaggarwalinseec/)"
+    "[GitHub](https://github.com/Towards-GenAI)"
+    
+    st.divider()
+    
+    upload_image = st.file_uploader("Upload Your Image Here", accept_multiple_files=False, type = ['jpg', 'png'])
+    
+    if upload_image:
+        image = Image.open(upload_image)
+
+    st.divider()
+
+    if st.button("Clear Chat History"):
+        st.session_state.messages.clear()
+        
+        
+#######################################################################################
+
+     
 
 
 
